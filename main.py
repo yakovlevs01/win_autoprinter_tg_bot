49a73af48534193aaed1dab3412d10295d6c601d
yakovlevs01 <yakovlevs01@yandex.ru>
2024-02-10 01:45:41 +0300

    now bot works with docx by converting them into pdf

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+import subprocess
 
 import telebot
 from dotenv import load_dotenv
 
 from pdf_creation import create_pdf
-from send_pdf_to_print import send_to_print
+from send_pdf_to_print import send_pdf_to_print
 
 load_dotenv()
 TG_BOT_TOKEN = os.getenv("TG_TOKEN")
@@ -16,10 +17,10 @@ allowed_users = (MY_TELEGRAM_ID, WIFE_TELEGRAM_ID)
 
 bot = telebot.TeleBot(TG_BOT_TOKEN)
 
-path_to_pdf_file = ""
+path_to_file = ""
 
 
-def pages_per_sheet_keyboard():
+def pages_per_sheet_keyboard() -> telebot.types.InlineKeyboardMarkup:
     return telebot.types.InlineKeyboardMarkup(
         keyboard=[
             [
@@ -31,7 +32,7 @@ def pages_per_sheet_keyboard():
     )
 
 
-def print_mode_keyboard():
+def print_mode_keyboard() -> telebot.types.InlineKeyboardMarkup:
     return telebot.types.InlineKeyboardMarkup(
         row_width=1,
         keyboard=[
@@ -50,14 +51,26 @@ def print_user_id(message):
 @bot.message_handler(content_types=["document"])
 @bot.message_handler(func=lambda msg: msg.from_user.id in allowed_users)
 def save_file(message):
-    global path_to_pdf_file
-    downloaded_file = bot.download_file(bot.get_file(message.document.file_id).file_path)
-    path_to_pdf_file = ".\\docs_to_print\\" + message.document.file_name
+    global path_to_file
 
-    with open(path_to_pdf_file, "wb") as new_file:
+    downloaded_file = bot.download_file(bot.get_file(message.document.file_id).file_path)
+    path_to_file = ".\\docs_to_print\\" + message.document.file_name
+
+    with open(path_to_file, "wb") as new_file:
         new_file.write(downloaded_file)
 
-    print(f"Downloaded the file {path_to_pdf_file}")
+    print(f"Downloaded the file {path_to_file}")
+
+    file_extension = path_to_file.split(".")[-1]
+    if file_extension == "docx":
+        final_path = ".".join(path_to_file.split(".")[:-1]) + ".pdf"
+        subprocess.run(["docx2pdf", path_to_file], check=True)
+        path_to_file = final_path
+    elif file_extension == "pdf":
+        pass
+    else:
+        bot.send_message(message.chat.id, "Такое я печатать не умею :(")
+        return
 
     bot.reply_to(message, "Сколько страниц на листе?", reply_markup=pages_per_sheet_keyboard())
     bot.send_message(message.chat.id, "Какая печать?", reply_markup=print_mode_keyboard())
@@ -65,7 +78,7 @@ def save_file(message):
 
 @bot.callback_query_handler(func=lambda call: call.data in {"1pps", "2pps", "4pps"})
 def callback_for_pps(call):
-    global path_to_pdf_file
+    global path_to_file
 
     print(f"User asked for {call.data[0]} pages per sheet")
 
@@ -78,7 +91,7 @@ def callback_for_pps(call):
     if call.data == "1pps":
         print("1pps, nothing to do")
     else:
-        path_to_pdf_file = create_pdf(path_to_pdf_file, pages_per_sheet=int(call.data[0]))
+        path_to_file = create_pdf(path_to_file, pages_per_sheet=int(call.data[0]))
 
 
 @bot.callback_query_handler(func=lambda call: call.data in {"simplex", "duplexlong", "duplexshort"})
@@ -92,7 +105,7 @@ def callback_of_print_mode(call):
         message_id=call.message.message_id,
     )
     print_mode = str(call.data)
-    send_to_print(path_to_pdf_file, mode=print_mode)
+    send_pdf_to_print(path_to_file, mode=print_mode)
 
 
 bot.infinity_polling()
--- send_pdf_to_print.py
+++ send_pdf_to_print.py
@@ -4,42 +4,46 @@ import win32api
 import win32print
 
 
-def deprecated_send_to_print(
-    pdf_file_path: str,
-    mode: int = 1,
+def send_any_doc_to_print(
+    file_path: str,
+    mode: str = "simplex",
     printer_name: str = win32print.GetDefaultPrinter(),  # Brother DCP-L2560DW series
 ):
-    """Sends pdf file to printer.
+    """Sends file to printer.
 
     Args:
-        pdf_file_path: string-path to file to print
+        file_path: string-path to file to print
 
-        mode: int - one of following values: 1, 2 or 3.
-                1 is single-sided printing (default)
+        mode: str - one of following values: "simplex", "duplexlong" or "duplexshort".
+                simplex is single-sided printing (default)
 
-                2 is double-sided printing with flipping on long edge (booklet layout)
+                duplexlong is double-sided printing with flipping on long edge (booklet layout)
 
-                3 is double-sided printing with flipping on short edge (tablet layout)
+                duplexshort is double-sided printing with flipping on short edge (tablet layout)
 
         printer_name: string-name of desired printer. Defaults to OS default printer.
 
     """
-    print(f"Sending pdf to {printer_name}")
+    modes_windows = {"simplex": 1, "duplexlong": 2, "duplexshort": 3}
+
+    print(f"Sending doc to {printer_name}")
+
     printdefaults = {"DesiredAccess": win32print.PRINTER_ALL_ACCESS}
     printer_handle = win32print.OpenPrinter(printer_name, printdefaults)
     printer_info = win32print.GetPrinter(printer_handle, 2)
     # режим печати: 1 - односторонняя, 2 двусторонняя по длинному краю, 3 - по короткому
     # https://learn.microsoft.com/ru-ru/windows/win32/api/wingdi/ns-wingdi-devmodea
-    printer_info["pDevMode"].Duplex = mode
+    printer_info["pDevMode"].Duplex = modes_windows[mode]
     win32print.SetPrinter(printer_handle, 2, printer_info, 0)
+
     print(f"Режим печати: {printer_info['pDevMode'].Duplex}")
 
-    win32api.ShellExecute(0, "print", pdf_file_path, None, ".", 0)
+    win32api.ShellExecute(0, "print", file_path, None, ".", 0)
 
     win32print.ClosePrinter(printer_handle)
 
 
-def send_to_print(pdf_file_path: str, mode: str = "simplex") -> None:
+def send_pdf_to_print(pdf_file_path: str, mode: str = "simplex") -> None:
     """Sends pdf file to printer.
 
     Args:
@@ -58,15 +62,15 @@ def send_to_print(pdf_file_path: str, mode: str = "simplex") -> None:
 
     print(
         subprocess.run(
-            f'{sumatra} -print-to-default -silent -print-settings "{mode}" "{pdf_file_path}"',
+            f'{sumatra} -print-to-default -silent -print-settings "{mode},noscale" "{pdf_file_path}"',
             shell=True,
             text=True,
             capture_output=True,
-        )
+            check=False,
+        ).returncode
     )
-    # print(subprocess.call([sumatra, "-print-to-default", "-silent", f'-print-settings "{mode}"', pdf_file_path]))
 
 
 if __name__ == "__main__":
     pdf_file_path = r".\test_files\test3.pdf"
-    send_to_print(pdf_file_path=pdf_file_path, mode=2)
+    send_pdf_to_print(pdf_file_path=pdf_file_path, mode=2)
